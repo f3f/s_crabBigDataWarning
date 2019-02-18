babcc41c7c597f239fdf748e1057af591308888e
prindarh <prindar@163.com>
2019-02-18 15:55:27 +0800

    hj

---
+++ static/config.js
@@ -0,0 +1,8 @@
+//export const BASEURL='/aobo'
+//export const BASEURL='http://192.168.21.31:8080/aobo'
+//export const BASEURL='http://192.168.20.153:8080/aobo'
+export const BASEURL='https://result.eolinker.com/jPvw7Wt21e26919677adc45a5a65456715247578444da37?uri='
+export const isStatic=true
+export const showLog=true
+export const BASEURLMOCK=''
+export const TIMEOUT=20000
